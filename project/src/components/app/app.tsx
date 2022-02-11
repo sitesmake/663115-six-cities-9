d15f097112e41296a52c3a44d54619c555b93caf
Alexander <sitesmake@gmail.com>
2022-02-11 20:43:39 +0300

    Made components from markup

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+
+type AppScreenProps = {
+  propertiesCount: number;
+}
+
+function App({propertiesCount}: AppScreenProps): JSX.Element {
+  return <MainScreen propertiesCount={propertiesCount} />;
 }
 
 export default App;
