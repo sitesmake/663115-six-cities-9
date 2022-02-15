a881349841ba78a2ad9c963b856f280b1d66b791
Alexander <sitesmake@gmail.com>
2022-02-15 15:21:02 +0300

    Add Routes

---
+++ project/src/const.ts
@@ -0,0 +1,6 @@
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer:id',
+}
---
+++ project/src/screens/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,25 @@
+import Header from '../../components/header/header';
+
+function LoginScreen(): JSX.Element {
+  return (
+    <div className="page page--gray page--login">
+      <Header />
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">Not Found.</h1>
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#">
+                <span>404 error</span>
+              </a>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default LoginScreen;
